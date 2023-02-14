2cd820103c87c332567569ef9a883f6b48f93fcb
Cristina-F-R <cristina.faixedas@gmail.com>
2023-02-14 09:31:36 +0100

    add function comparison

--- js/main.js
+++ js/main.js
@@ -9,15 +9,30 @@ function getRandomNumber(max) {
  return Math.ceil(Math.random() * max);
 }
 
-console.log('Tu número aleatorio es: '+ getRandomNumber(100));
+const randomNumber = getRandomNumber(100);
+console.log('Tu número aleatorio es: '+ randomNumber);
 
 handleclick = (e) =>{
     e.preventDefault();
-    let inputValue = input.value;
-    console.log(inputValue);
+    // let inputValue = input.value;
+    // console.log(inputValue);
     comparison();
 }
 
-
+function comparison() {
+    let inputValue = parseInt(input.value);
+    console.log(randomNumber);
+    if(inputValue > 100 || inputValue < 0){
+       clue.innerHTML = 'El número debe estar entre 1 y 100.' 
+    }if (inputValue === randomNumber){
+        clue.innerHTML = 'Has ganado campeona!!!';
+    }if (inputValue > randomNumber){
+        clue.innerHTML = 'Demasiado alto';
+    }if (inputValue < randomNumber){
+        clue.innerHTML = 'Demasiado bajo';
+    }else{
+        console.log('error');
+    }
+}
 
 btn.addEventListener('click', handleclick);
